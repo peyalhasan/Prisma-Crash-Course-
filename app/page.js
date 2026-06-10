654e8b81cb7931c805c6dbb67f5368cb19cf6df8
Peyal Hasan <peyalhasan143@gmail.com>
2026-06-11 00:21:19 +0600

    fix: remove prisma.config.ts, fix moduleResolution, add force-dynamic

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import { getPins } from "./actions";
 import PinClient from "./PinClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const pins = await getPins();
   return <PinClient pins={pins} />;
